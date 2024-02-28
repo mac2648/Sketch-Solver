82ce7fb17b4789bd530114685e9177896f438c65
Soomin Lee <whonineofive@gmail.com>
2024-02-28 17:26:42 -0500

    first commit

---
+++ Source/SketchSolver/SketchSolverGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SketchSolverGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASketchSolverGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASketchSolverGameMode();
+};
+
+
+
